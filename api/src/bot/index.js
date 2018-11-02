87c7b27cf4988bc21dd086c6b9ec774e473697e9
mdshun <hoang_van_tuan@mediado.jp>
2018-11-03 08:02:51 +0900

    add log

--- api/src/bot/index.js
+++ api/src/bot/index.js
@@ -4,7 +4,11 @@ const log = require('../log')
 const util = require('../util')
 
 module.exports.BotAPI = async teamID => {
+    log.debug('create bot api for team id', teamID)
+
     const res = await db.SlackTeam.get({ team_id: teamID })
+
+    log.debug(res.data)
 
     if (res.data.statusText === 'ok' && res.data.data.length === 1) {
         const team = res.data.data[0]
@@ -12,6 +16,5 @@ module.exports.BotAPI = async teamID => {
         return new Slack({ token: util.Decode(team.bot.bot_access_token) })
     }
 
-    log.error(res.data.data)
     throw new Error('can not create slack api client')
 }
